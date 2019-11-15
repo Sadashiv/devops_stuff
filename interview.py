b6a7e506cfccf64f330f711f28e10fba28e19eba
Sadashiv Badagi <sadamsrit@gmail.com>
2019-11-15 20:33:45 +0530

    Search and replace script

--- interview.py
+++ interview.py
@@ -78,3 +78,12 @@ def greater_than_5(x):
     return x > 5
 
 #filter(greater_than_5, range[10])
+fr = open('class.py', 'r')
+fw = fr.readlines()
+fr.close()
+fa = open('class.py', 'a')
+for k in fw:
+    if 'add' in k:
+        fa.write(k.replace('add', 'kkkk'))
+fa.close()
+
